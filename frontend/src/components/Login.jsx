4f18cdb54c624d4ba834bf57844539eb5601551f
Ashwinn11 <ashwinnanbazhagan@gmail.com>
2024-07-19 18:47:59 +0530

    added oauth

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+const Login = () => {
+  const responseGoogle = (response)=>{
+    console.log(response);
+  }
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5" width="130px" alt="logo">
+            <img src={logo}></img>
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy='single-host-origin'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;

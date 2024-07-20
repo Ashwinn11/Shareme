fa683e896df0ab28e4f713b91fb64d4daf2004e7
Ashwinn11 <ashwinnanbazhagan@gmail.com>
2024-07-20 15:54:16 +0530

    local storage integrate

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { useGoogleLogin } from "@react-oauth/google";
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
-import axios from 'axios';
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
-  const responseGoogle = (response) => {
-    console.log(response);
-  };
+  const googleResponse = useGoogleLogin({
+    onSuccess: async (response) => {
+      const userInfo = await axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: {
+            Authorization: `Bearer ${response.access_token}`,
+          },
+        })
+        .then((res) => res.data);
+
+      console.log(userInfo);
+      localStorage.setItem('user',JSON.stringify(userInfo))
+    },
+    onError: (error)=>{
+      console.log(error);
+    }
+
+  });
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -28,22 +44,14 @@ const Login = () => {
             <img src={logo} width="130px" alt="logo"></img>
           </div>
           <div className="shadow-2xl">
-           <button type="button" 
-           className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none" 
-           onClick={useGoogleLogin({
-            onSuccess: async response=>{
-              console.log(response)
-              const userInfo = await axios
-    .get('https://www.googleapis.com/oauth2/v3/userinfo', {
-      headers: { Authorization: `Bearer ${response.access_token}` },
-    })
-    .then(res => res.data);
-
-  console.log(userInfo);
-            }
-
-           })}>
-            <FcGoogle className="mr-4"/>Sign in with google </button>
+            <button
+              type="button"
+              className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+              onClick={()=>googleResponse()}
+            >
+              <FcGoogle className="mr-4" />
+              Sign in with google
+            </button>
           </div>
         </div>
       </div>
